8e9006f48b284b50e9545d827953cc81c558fbfa
Mert Gunduz <merttgg@gmail.com>
2021-08-17 06:30:25 +0300

    Project Started

---
+++ WordTown_MainMenu.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace WordTown
+{
+    partial class WordTown_MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WordTown_MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "WordTown_MainMenu";
+            this.Text = "WordTown";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
